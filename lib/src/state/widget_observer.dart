e3d470ec28d40e586ae1e7a82d175e87910991fd
kagwicharles <kagwitheuri@gmail.com>
2023-05-18 15:40:55 +0300

    refactorcode

--- lib/src/state/widget_observer.dart
+++ lib/src/state/widget_observer.dart
@@ -1,7 +1,5 @@
 import 'package:craft_dynamic/craft_dynamic.dart';
-import 'package:craft_dynamic/src/state/plugin_state.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class AppLifecycleObserver extends WidgetsBindingObserver {
   final BuildContext context;
@@ -13,10 +11,8 @@ class AppLifecycleObserver extends WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     super.didChangeAppLifecycleState(state);
 
-    final pluginState = Provider.of<PluginState>(context, listen: false);
     if (state == AppLifecycleState.paused) {
-      CommonUtils.getXRouteAndPopAll(
-          widget: widget ?? pluginState.logoutScreen);
+      CommonUtils.getXRouteAndPopAll(widget: widget);
     }
   }
 }
