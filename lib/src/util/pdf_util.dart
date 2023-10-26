35868fe946c6c83662e0434084899e55d4a759a5
kagwicharles <kagwitheuri@gmail.com>
2023-10-26 20:22:48 +0300

    update: refactor pdf util

--- lib/src/util/pdf_util.dart
+++ lib/src/util/pdf_util.dart
@@ -6,7 +6,6 @@ class PDFUtil {
       Map<String, dynamic>? receiptdetails,
       downloadReceipt = true,
       isShare = false}) async {
-    String receiptNo = "";
     Color color = APIService.appPrimaryColor;
     final profileRepo = ProfileRepository();
     final PdfDocument document = PdfDocument();
@@ -116,70 +115,9 @@ class PDFUtil {
       }
     }
 
-    PdfGrid grid = PdfGrid();
-    var gridStyle = PdfGridStyle(
-      cellPadding: PdfPaddings(left: 20, right: 20, top: 8, bottom: 8),
-      backgroundBrush: PdfBrushes.transparent,
-      textBrush: PdfBrushes.black,
-      font: PdfStandardFont(PdfFontFamily.helvetica, 18),
-    );
-    grid.style = gridStyle;
-    grid.columns.add(count: 2);
+    page.graphics.restore(await drawWaterMark(page));
 
-    (postDynamic?.receiptDetails?.isEmpty ?? true)
-        ? receiptdetails?.entries.forEach((item) {
-            PdfGridRow row = grid.rows.add();
-            String title = item.key;
-            String value = row.cells[1].value = item.value ?? "****";
-            if (title == "Reference No") {
-              receiptNo = value;
-            }
-            row.cells[0].value = title;
-            row.cells[1].value = value;
-          })
-        : postDynamic?.receiptDetails?.asMap().forEach((index, item) {
-            PdfGridRow row = grid.rows.add();
-            String title =
-                MapItem.fromJson(postDynamic.receiptDetails?[index]).title;
-            String value = row.cells[1].value =
-                MapItem.fromJson(postDynamic.receiptDetails?[index]).value ??
-                    "****";
-            if (title == "Reference No") {
-              receiptNo = value;
-            }
-            row.cells[0].value = title;
-            row.cells[1].value = value;
-          });
-
-    //Watermark image
-    ByteData imageBytes = await rootBundle.load('assets/launcher.png');
-    List<int> imageBitmap = imageBytes.buffer.asUint8List();
-
-    PdfGraphicsState state = page.graphics.save();
-    page.graphics.setTransparency(0.25);
-    page.graphics.drawImage(
-        PdfBitmap.fromBase64String(base64.encode(imageBitmap)),
-        Rect.fromLTWH((page.graphics.clientSize.width / 2) - 150,
-            (page.graphics.clientSize.height / 2) - 150, 300, 300));
-    page.graphics.restore(state);
-
-    // Set margins for the page
-    double leftMargin = 0; // Adjust the left margin value as desired
-    double rightMargin = 0; // Adjust the right margin value as desired
-    double topMargin = 20 +
-        hiTextUpperBound +
-        subTitleUpperBound; // Adjust the top margin value as desired
-    double bottomMargin = 0; // A
-
-    PdfLayoutResult? layoutResult = grid.draw(
-      page: page,
-      bounds: Rect.fromLTWH(
-        leftMargin,
-        topMargin,
-        page.getClientSize().width - (leftMargin + rightMargin),
-        0,
-      ),
-    );
+    String receiptNo = await drawGrid(page, postDynamic, receiptdetails);
 
     String receiptname = "Receipt$receiptNo";
     String filePath = "${directory?.path}/$receiptname.pdf";
@@ -206,6 +144,79 @@ class PDFUtil {
     if (details.containsKey("TransType")) {
       return details["TransType"];
     }
+    return null;
+  }
+
+  static Future<String> drawGrid(PdfPage page, PostDynamic? postDynamic,
+      Map<String, dynamic>? receiptdetails,
+      {double hiTextUpperBound = 60, subTitleUpperBound = 60 + 50}) async {
+    PdfGrid grid = PdfGrid();
+    var gridStyle = PdfGridStyle(
+      cellPadding: PdfPaddings(left: 20, right: 20, top: 8, bottom: 8),
+      backgroundBrush: PdfBrushes.transparent,
+      textBrush: PdfBrushes.black,
+      font: PdfStandardFont(PdfFontFamily.helvetica, 18),
+    );
+    grid.style = gridStyle;
+    grid.columns.add(count: 2);
+    // Set margins for the page
+    double leftMargin = 0; // Adjust the left margin value as desired
+    double rightMargin = 0; // Adjust the right margin value as desired
+    double topMargin = 20 +
+        hiTextUpperBound +
+        subTitleUpperBound; // Adjust the top margin value as desired
+    String receiptNo = ""; // A
+
+    (postDynamic?.receiptDetails?.isEmpty ?? true)
+        ? receiptdetails?.entries.forEach((item) {
+            PdfGridRow row = grid.rows.add();
+            String title = item.key;
+            String value = row.cells[1].value = item.value ?? "****";
+            if (title.toLowerCase() == "reference no" ||
+                title.toLowerCase() == "bankreference") {
+              receiptNo = value;
+            }
+            row.cells[0].value = title;
+            row.cells[1].value = value;
+          })
+        : postDynamic?.receiptDetails?.asMap().forEach((index, item) {
+            PdfGridRow row = grid.rows.add();
+            String title =
+                MapItem.fromJson(postDynamic.receiptDetails?[index]).title;
+            String value = row.cells[1].value =
+                MapItem.fromJson(postDynamic.receiptDetails?[index]).value ??
+                    "****";
+            if (title.toLowerCase() == "reference no" ||
+                title.toLowerCase() == "bankreference") {
+              receiptNo = value;
+            }
+            row.cells[0].value = title;
+            row.cells[1].value = value;
+          });
+
+    PdfLayoutResult? layoutResult = grid.draw(
+      page: page,
+      bounds: Rect.fromLTWH(
+        leftMargin,
+        topMargin,
+        page.getClientSize().width - (leftMargin + rightMargin),
+        0,
+      ),
+    );
+    return receiptNo;
+  }
+
+  static Future<PdfGraphicsState> drawWaterMark(PdfPage page) async {
+    //Watermark image
+    ByteData imageBytes = await rootBundle.load('assets/launcher.png');
+    List<int> imageBitmap = imageBytes.buffer.asUint8List();
+    PdfGraphicsState state = page.graphics.save();
+    page.graphics.setTransparency(0.25);
+    page.graphics.drawImage(
+        PdfBitmap.fromBase64String(base64.encode(imageBitmap)),
+        Rect.fromLTWH((page.graphics.clientSize.width / 2) - 150,
+            (page.graphics.clientSize.height / 2) - 150, 300, 300));
+    return state;
   }
 }
 
