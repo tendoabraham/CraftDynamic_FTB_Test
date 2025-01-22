4493663e63aa29359fadbeb1825d2b9bbc754ad4
Tendo Abraham <tendoabraham@gmail.com>
2025-01-23 01:01:42 +0300

    connectivity_plus

--- lib/src/network/connectivity_plus.dart
+++ lib/src/network/connectivity_plus.dart
@@ -3,24 +3,53 @@ import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:craft_dynamic/craft_dynamic.dart';
 
+// class ConnectivityService {
+//   final Connectivity _connectivity = Connectivity();
+//   StreamController<ConnectivityResult> connectionStatusController =
+//       StreamController<ConnectivityResult>();
+
+//   Future<void> initialize() async {
+//     ConnectivityResult result = await _connectivity.checkConnectivity();
+//     connectionStatusController.add(result);
+
+//     _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+//       AppLogger.appLogD(
+//           tag: "connection changed", message: "connection state has changed");
+//       connectionState.value = result;
+//       connectionStatusController.add(result);
+//     });
+//   }
+
+//   void dispose() {
+//     connectionStatusController.close();
+//   }
+// }
+
 class ConnectivityService {
   final Connectivity _connectivity = Connectivity();
-  StreamController<ConnectivityResult> connectionStatusController =
-      StreamController<ConnectivityResult>();
+  final StreamController<List<ConnectivityResult>> connectionStatusController =
+      StreamController<List<ConnectivityResult>>.broadcast();
+
+  late StreamSubscription<List<ConnectivityResult>> _subscription;
 
   Future<void> initialize() async {
-    ConnectivityResult result = await _connectivity.checkConnectivity();
-    connectionStatusController.add(result);
+    // Check the current connectivity state
+    List<ConnectivityResult> results = await _connectivity.checkConnectivity();
+    connectionStatusController.add(results);
 
-    _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
-      AppLogger.appLogD(
-          tag: "connection changed", message: "connection state has changed");
-      connectionState.value = result;
-      connectionStatusController.add(result);
+    // Listen for connectivity changes
+    _subscription = _connectivity.onConnectivityChanged
+        .listen((List<ConnectivityResult> results) {
+      print("Connection state has changed to: $results");
+      connectionStatusController.add(results);
     });
   }
 
   void dispose() {
+    // Cancel the stream subscription
+    _subscription.cancel();
+
+    // Close the connectionStatusController
     connectionStatusController.close();
   }
 }
