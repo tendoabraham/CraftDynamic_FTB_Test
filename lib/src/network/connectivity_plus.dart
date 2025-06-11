b2177c3d48896fcea3dd3e3823a2e3be6e12bf8d
Tendo Abraham <tendoabraham@gmail.com>
2025-06-11 09:57:36 +0300

    connect plus

--- lib/src/network/connectivity_plus.dart
+++ lib/src/network/connectivity_plus.dart
@@ -2,28 +2,59 @@ import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:craft_dynamic/craft_dynamic.dart';
+import 'package:flutter/foundation.dart';
 
 class ConnectivityService {
   final Connectivity _connectivity = Connectivity();
-  StreamController<ConnectivityResult> connectionStatusController =
-      StreamController<ConnectivityResult>();
+  final StreamController<ConnectivityResult> connectionStatusController =
+      StreamController<ConnectivityResult>.broadcast();
+
+  // Optional: expose current state with ValueNotifier
+  final ValueNotifier<ConnectivityResult> connectionState =
+      ValueNotifier<ConnectivityResult>(ConnectivityResult.none);
 
   Future<void> initialize() async {
-    ConnectivityResult result = await _connectivity.checkConnectivity();
+    final ConnectivityResult result =
+        (await _connectivity.checkConnectivity()) as ConnectivityResult;
+    connectionState.value = result;
     connectionStatusController.add(result);
 
     _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
       AppLogger.appLogD(
-          tag: "connection changed", message: "connection state has changed");
+        tag: "connection changed",
+        message: "Connection state has changed to: $result",
+      );
       connectionState.value = result;
       connectionStatusController.add(result);
-    });
+    } as void Function(List<ConnectivityResult> event)?);
   }
 
   void dispose() {
     connectionStatusController.close();
   }
 }
+
+// class ConnectivityService {
+//   final Connectivity _connectivity = Connectivity();
+//   StreamController<ConnectivityResult> connectionStatusController =
+//       StreamController<ConnectivityResult>();
+
+//   Future<void> initialize() async {
+//     ConnectivityResult result = await _connectivity.checkConnectivity();
+//     connectionStatusController.add(result);
+
+//     _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+//       AppLogger.appLogD(
+//           tag: "connection changed", message: "connection state has changed");
+//       connectionState.value = result;
+//       connectionStatusController.add(result);
+//     });
+//   }
+
+//   void dispose() {
+//     connectionStatusController.close();
+//   }
+// }
 
 // class ConnectivityService {
 //   final Connectivity _connectivity = Connectivity();
